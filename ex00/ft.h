debbb0832731bd27b5f99a6c59aa8cea0776ade1
Tashreeq Edwards <tedwards@c4r11s3.wethinkcode.co.za>
2020-06-26 14:11:27 +0200

    HELL

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,8 +6,20 @@
 /*   By: tedwards <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 09:42:38 by tedwards          #+#    #+#             */
-/*   Updated: 2020/06/26 09:43:16 by tedwards         ###   ########.fr       */
+/*   Updated: 2020/06/26 14:09:52 by tedwards         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include <libft.h
+
+
+int		ft_putchar(char c);
+
+void	ft_putstr(char *str);
+
+void	ft_swap(int	*a,	int	*b);
+
+int	ft_strcmp( char *s1, char *s2);
+
+int	ft_strlen(char *str);	
+>
